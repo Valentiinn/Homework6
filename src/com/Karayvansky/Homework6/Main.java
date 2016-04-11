421404c89787f8f25326032b5ca807f85a2bf735
Valentin Karayvansky <Valik-isuzu@mail.ru>
2016-04-11 15:43:34 +0300

    First commit

---
+++ src/com/Karayvansky/Homework6/Main.java
@@ -0,0 +1,15 @@
+package com.Karayvansky.Homework6;
+
+import com.Karayvansky.Homework6.Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int firstNumber = Integer.parseInt(args[0]);
+		int secondNumber = Integer.parseInt(args[1]);
+		int result = firstNumber + secondNumber;
+		System.out.println("Result addition = " + result);
+		
+	}
+
+}
